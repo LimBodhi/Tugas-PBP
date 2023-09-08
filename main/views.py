6ebb4b967f9b22e80c1eeccb66f9ff78c879fb75
Lim Bodhi Wijaya <lim.bodhi@ui.ac.id>
2023-09-08 20:32:09 +0700

    Tugas 2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Barang 1',
+        'amount': 100,
+        'price': 10000,
+        'description': "Ini barang 1"
+    }
+
+    return render(request, "main.html", context)
